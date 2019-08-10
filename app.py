71789a591b3f9e0a39f9245f10396f528a756d62
Ubuntu <ubuntu@ip-172-31-35-239.ec2.internal>
2019-08-10 17:44:29 +0000

    Adding ability to create recipe

--- app.py
+++ app.py
@@ -7,13 +7,26 @@ app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'cookbookDb'
 app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
-
 mongo = PyMongo(app)
 
 @app.route('/')
 @app.route('/get_top_recipes')
 def get_top_recipes():
     return render_template("home.html", recipes=mongo.db.recipes.find())
+
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    recipes = mongo.db.recipes
+    recipes.insert_one(request.form.to_dict())
+    return redirect(url_for('get_top_recipes'))
+    
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template("add_recipe.html")
+    
+@app.route('/get_recipe_names')
+def get_recipe_names():
+    return render_template("index.html", names=mongo.db.recipe_names.find())
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
