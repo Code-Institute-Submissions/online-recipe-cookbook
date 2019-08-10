99ea691d0317663a018a699ac94adc5a7091c42c
Ubuntu <ubuntu@ip-172-31-35-239.ec2.internal>
2019-08-10 08:15:06 +0000

    adding app.py

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'cookbookDb'
 app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
 mongo = PyMongo(app)
+
+
+@app.route('/')
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("index.html")
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
