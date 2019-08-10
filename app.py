dee924f4e0b3c4713a4db9a9b15e94991745e02b
Ubuntu <ubuntu@ip-172-31-35-239.ec2.internal>
2019-08-10 20:43:12 +0000

    Allow user to edit a recipe

--- app.py
+++ app.py
@@ -7,12 +7,35 @@ app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'cookbookDb'
 app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
+
+
 mongo = PyMongo(app)
 
 @app.route('/')
 @app.route('/get_top_recipes')
 def get_top_recipes():
     return render_template("home.html", recipes=mongo.db.recipes.find())
+
+@app.route('/edit_recipe/<recipe_id>')
+def edit_recipe(recipe_id):
+    the_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    return render_template('edit_recipe.html', recipe=the_recipe)
+
+@app.route('/update_recipe/<recipe_id>', methods=['POST'])
+def update_recipe(recipe_id):
+    recipes = mongo.db.recipes
+    recipes.update({'_id': ObjectId(recipe_id)},
+    {
+        'recipe_name':request.form.get('recipe_name'),
+        'prep_time':request.form.get('prep_time'),
+        'cook_time':request.form.get('cook_time'),
+        'ingredients':request.form.get('ingredients'),
+        #'cuisine':request.form.get('cuisine'),
+        'method_steps':request.form.get('method_steps'),
+        'tools_used':request.form.get('tools_used'),
+        'image_link':request.form.get('image_link')
+    })
+    return redirect(url_for('get_top_recipes'))
 
 @app.route('/insert_recipe', methods=['POST'])
 def insert_recipe():
